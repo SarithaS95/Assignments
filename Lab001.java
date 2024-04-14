7aeffcf7eb47767c732633888ba0bc69f82a5a5c
SarithaS95 <saritha.surampudi@gmail.com>
2024-04-14 16:14:12 +0530

    first commit

---
+++ Lab001.java
@@ -0,0 +1,26 @@
+package E_041124;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        short i = 65;
+        int a=190;
+        char b='h';
+        String c="Saritha";
+        boolean t= true;
+        double e= 0.1;
+        float f=0.1f; // f has to be given along with value
+        long g= 8500792965l;
+        //"/n"- new line, println- new line, print- on same line, printf- customization of data to be printed in console
+        // byte= -128 to 127 range
+        // Primitive data type - can be broken further
+        // short= -31567 to 32000
+        //int = -31k to -32k
+        // long- more than 10 digit number - should add L at the end of value
+        //float= 3.14f
+        //Non primitive data types: class ,arrays, interface
+        // 8 bytes for these non-primitive data types
+
+
+        System.out.println(i);
+    }
+}
